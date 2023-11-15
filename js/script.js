979e1761883012f04c5ebf416a139b1c3aecb0bd
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-11-15 18:51:43 +0100

    fix media player

--- js/script.js
+++ js/script.js
@@ -38,10 +38,11 @@ contatti.onclick = () => midRight.classList.remove("d-none");
 ///per controlli media playerd
 const audio = document.getElementById("audio");
 console.log(audio);
+
 console.log(audio.duration);
 console.log(audio.currentTime);
-const playBtns = Array.from(document.getElementsByClassName("play-btn"));
-console.log(playBtns[0].innerHTML);
+const playBtns = Array.from(document.getElementsByClassName("fa-play"));
+console.log(playBtns[0]);
 const currentTime = document.getElementById("current-time-music");
 console.log(currentTime);
 const durationTime = document.getElementById("duration-time-music");
@@ -50,38 +51,61 @@ const rangePlayer = document.getElementById("range-player");
 console.log(rangePlayer);
 const volumePlayer = document.getElementById("volume");
 console.log(volume);
+
 // const nextSongBtn = document.getElementById("audio");
 // console.log(nextSongBtn);
 // const previusSongBtn = document.getElementById("audio");
 // console.log(previusSongBtn);
 
-const playAudio = btn => {
+const playAudio = () => {
   audio.play();
-  playBtns.forEach(btn => (btn.innerHTML = '<i class="fas fa-pause"></i'));
+  console.log("spaghetti");
+  playBtns.forEach(btn => {
+    btn.classList.remove("fa-play");
+    btn.classList.add("fa-pause");
+    console.log(playBtns);
+  });
 };
 
-const stopAudio = btn => {
+const stopAudio = () => {
   audio.pause();
-  audio.currentTime = 0; // Reset the audio to the beginning
-  playBtns.forEach(btn => (btn.innerHTML = '<i class="fas fa-play"></i>'));
+  console.log("maccheroni");
+  audio.currentTime = 0;
+  playBtns.forEach(btn => {
+    btn.classList.remove("fa-pause");
+    btn.classList.add("fa-play");
+    console.log(playBtns);
+  });
 };
-
 playBtns.forEach(btn => {
   btn.addEventListener("click", function () {
-    // Check the inner HTML to determine if it's a play or pause button
-    if (btn.innerHTML === '<i class="fas fa-play"></i>') {
-      playAudio(btn);
+    if (btn.classList.contains("fa-play")) {
+      playAudio();
     } else {
-      stopAudio(btn);
+      stopAudio();
     }
   });
 });
-////aggioramento barra audio
 
+////aggioramento barra audio
+const minTime = function (seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (remainingSeconds < 10) {
+    remainingSeconds = "0" + remainingSeconds;
+  }
+
+  return minutes + ":" + remainingSeconds;
+};
 const updateMediaPlayer = () => {
-  currentTime.innerText = Math.floor(audio.currentTime);
-  durationTime.innerText =
-    Math.floor(audio.duration) - Math.floor(audio.currentTime);
+  currentTime.innerText = minTime(Math.floor(audio.currentTime));
+  durationTime.innerText = minTime(
+    Math.floor(audio.duration) - Math.floor(audio.currentTime)
+  );
   rangePlayer.value = (audio.currentTime / audio.duration) * 100;
 };
 const time = setInterval(updateMediaPlayer, 100);
